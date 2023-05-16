5c57d0eb64cb7a1175381e1e3187dcadf756e379
Irene Brignoli <ire.brignoli@gmail.com>
2023-05-16 15:25:25 +0200

    first commit

---
+++ app/Http/Controllers/Guest/Pagecontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Pagecontroller extends Controller
+{
+    public function index(){
+
+        return view('home');
+    }
+}
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+  <div class="text-center">Boolean comics</div>
+</footer>
