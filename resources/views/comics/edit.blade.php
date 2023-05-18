91c364c929285714934e8ae5106ba4dc6aef77d2
Irene Brignoli <ire.brignoli@gmail.com>
2023-05-18 17:04:23 +0200

    sistemato con validation anche metodo update e pag edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -11,37 +11,73 @@
 
   <div class="mb-3">
       <label for="title" class="form-label">Title:</label>
-      <input type="text" class="form-control" id="title" name="title" value="{{$comic->title}}">
+      <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" 
+      value="{{old('title')?old('title'):$comic->title}}">
+
+      @error('title')
+        <div class="alert alert-danger mt-2">{{ $message }}</div>
+      @enderror
   </div>
 
   <div class="mb-3">
       <label for="description" class="form-label">Description:</label>
-      <textarea type="text" class="form-control" id="description" name="description" placeholder="optional...">{{$comic->description}}</textarea>
+      <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description" placeholder="optional...">
+        {{old('description')?old('description'):$comic->description}}
+       </textarea>
+
+      @error('description')
+        <div class="alert alert-danger mt-2">{{ $message }}</div>
+      @enderror
   </div>
 
   <div class="mb-3">
       <label for="thumb" class="form-label">Image url</label>
-      <input type="text" class="form-control" id="thumb" name="thumb" placeholder="optional..." value="{{$comic->thumb}}">
+      <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" placeholder="optional..." 
+      value="{{old('thumb')?old('thumb'):$comic->thumb}}">
+
+      @error('thumb')
+        <div class="alert alert-danger mt-2">{{ $message }}</div>
+      @enderror
   </div>
 
   <div class="mb-3">
       <label for="price" class="form-label">Price:</label>
-      <input type="text" class="form-control" id="price" name="price" value="{{$comic->price}}">
+      <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price"
+      value="{{old('price')?old('price'):$comic->price}}">
+
+      @error('price')
+        <div class="alert alert-danger mt-2">{{ $message }}</div>
+      @enderror
   </div>
 
   <div class="mb-3">
       <label for="series" class="form-label">Series:</label>
-      <input type="text" class="form-control" id="series" name="series" value="{{$comic->series}}">
+      <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series"
+      value="{{old('series')?old('series'):$comic->series}}">
+
+      @error('series')
+         <div class="alert alert-danger mt-2">{{ $message }}</div>
+      @enderror
   </div>
 
   <div class="mb-3">
       <label for="sale_date" class="form-label">Sale date:</label>
-      <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
+      <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date"
+      value="{{old('sale_date')?old('sale_date'):$comic->sale_date}}">
+
+      @error('sale_date')
+        <div class="alert alert-danger mt-2">{{ $message }}</div>
+      @enderror
   </div>
 
   <div class="mb-3">
     <label for="type" class="form-label">Type:</label>
-    <input type="text" class="form-control" id="type" name="type" value="{{$comic->type}}">
+    <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type"
+    value="{{old('type')?old('type'):$comic->type}}">
+
+    @error('type')
+        <div class="alert alert-danger mt-2">{{ $message }}</div>
+    @enderror
 </div>
 
   <button type="submit" class="btn btn-primary">Save data</button>
